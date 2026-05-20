779eca6d214f2232f87146b56d10d775c0845076
monixvision <garciasantanamonica@gmail.com>
2026-05-20 20:02:23 +0200

    hemos creado el Header copiado y pegado modificando de forma que esté el return implicito

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => (
+    <header className={styles.header}>
+      <h1>FrameApp</h1>
+      <nav>
+        <a href="/">Inicio</a>  
+        <a href="/catalogo">Catálogo</a>
+        <a href="/contacto">Nosotros</a>
+      </nav>
+    </header>
+);
+
+export default Header;
