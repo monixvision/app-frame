a869aabd8421c921c3b5122b5aa56a329345397b
monixvision <garciasantanamonica@gmail.com>
2026-05-25 21:15:18 +0200

    hemos enrutado todas las pages a la app y luego con el link a cada una de las rutas en el bottom bard

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Home from './pages/Home';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Category from './pages/Category';
+import Actividades from './pages/Actividades';
+import Descargas from './pages/Descargas';
+import Faq from './pages/FAQ';
+import Nosotros from './pages/Nosotros';
+import Entradas from './pages/Entradas';
 
 const App = () => (
     <BrowserRouter> {/* Esto permite la navegación entre rutas */}
@@ -11,7 +16,13 @@ const App = () => (
       <main>
         <Routes> {/* Aqui se definen las rutas */}
           <Route index element={<Home />} />  {/* Ruta principal */}
-          <Route path="/peliculas" element={<Category />}/> {/* Ruta para la página de películas que tiene que coincidir en el buscador y justo enseñar el elemento Category solo que la ruta se llama distinto eso no importa le puedes poner el nombre que quieras con tal que coja el elemento en concreto! */}
+          <Route path="/peliculas" element={<Category />}/> 
+          {/* Ruta para la página de películas que tiene que coincidir en el buscador y justo enseñar el elemento Category solo que la ruta se llama distinto eso no importa le puedes poner el nombre que quieras con tal que coja el elemento en concreto! */}
+          <Route path="/actividades" element={<Actividades />}/> 
+          <Route path="/descargas" element={<Descargas />}/>
+          <Route path="/preguntas" element={<Faq />}/>
+          <Route path="/nosotros" element={<Nosotros />}/>
+          <Route path="/entradas" element={<Entradas />}/>
         </Routes>
       </main>
       <Footer />
--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -12,18 +12,18 @@ const BottomBar = () => (
             <ul className="text-center flex flex-col gap-4 text-white text-xl py-5">
                 <li><Link to='/peliculas'>Películas</Link></li>
                 {/* en el link to tiene que coincidir con la ruta definida en App.jsx */}
-                <li><a href="#">Actividades</a></li>
-                <li><a href="#">Nosotros</a></li>
-                <li><a href="#">Descargas</a></li>
+                <li><Link to='/actividades'>Actividades</Link></li>
+                <li><Link to='/nosotros'>Nosotros</Link></li>
+                <li><Link to='/descargas'>Descargas</Link></li>
             </ul>
         </div>
 
         <ul className="py-3 px-6 gap-6 flex align-center justify-center rounded-full bg-indigo-600">
-            <li><img src={icoCalendar} alt="Calendario" /></li>
-            <li><img src={icoHelp} alt="Ayuda" /></li>
-            <li><img src={icoBurguer} alt="Menú" /></li>
+            <li><Link to='/actividades'><img src={icoCalendar} alt="Calendario" /></Link></li>
+            <li><Link to='/preguntas'><img src={icoHelp} alt="Ayuda" /></Link></li>
+            <li><Link to='/menu'><img src={icoBurguer} alt="Menú" /></Link></li>
             <li><Link to='/peliculas'><img src={icoMovie} alt="Películas" /></Link></li>
-            <li><img src={icoTicket} alt="Entradas" /></li>
+            <li><Link to='/entradas'><img src={icoTicket} alt="Entradas" /></Link></li>
         </ul>
     </nav>
     </div>
