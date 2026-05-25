8fe04b759a7b4eeb0440747f80d63cfbe5c84b32
monixvision <garciasantanamonica@gmail.com>
2026-05-25 20:28:36 +0200

    enrutamos todos los enlaces internos que llevan a /peliculas tanto el buttombar como los iconos y he arreglado las rutas de las imagenes pq tenia guiones en vez de barra bajas en los nombres de los elementos

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import icoBurguer from '../assets/img/ico-burger.svg';
 import icoCalendar from '../assets/img/ico-calendar.svg';
 import icoHelp from '../assets/img/ico-help.svg';
@@ -9,7 +10,8 @@ const BottomBar = () => (
     <nav className="flex flex-col gap-3">
         <div className="bg-indigo-600 rounded-xl p-4 shadow-xl">
             <ul className="text-center flex flex-col gap-4 text-white text-xl py-5">
-                <li><a href="#">Películas</a></li>
+                <li><Link to='/peliculas'>Películas</Link></li>
+                {/* en el link to tiene que coincidir con la ruta definida en App.jsx */}
                 <li><a href="#">Actividades</a></li>
                 <li><a href="#">Nosotros</a></li>
                 <li><a href="#">Descargas</a></li>
@@ -20,7 +22,7 @@ const BottomBar = () => (
             <li><img src={icoCalendar} alt="Calendario" /></li>
             <li><img src={icoHelp} alt="Ayuda" /></li>
             <li><img src={icoBurguer} alt="Menú" /></li>
-            <li><img src={icoMovie} alt="Películas" /></li>
+            <li><Link to='/peliculas'><img src={icoMovie} alt="Películas" /></Link></li>
             <li><img src={icoTicket} alt="Entradas" /></li>
         </ul>
     </nav>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import logoBlanco from '../assets/img/FRAME-logowhite.svg';
 import BottomBar from './BottomBar';
 
 const Header = () => (
     <header className="bg-zinc-700 p-2">
-        <a href="/">
+        <Link to="/">
             <img src={logoBlanco} alt="Logo de la empresa" className= {`${styles.cabecera__logo} mx-auto`} />
-        </a>
+        </Link>
         <BottomBar />
     </header>
 );
