fd2d351eef6020dacb97853ca82fff885e07080a
monixvision <garciasantanamonica@gmail.com>
2026-05-25 19:56:50 +0200

    hemos creado el menu desplegable, importado la fuente y agregado una nroma para la ia que es que ponga siempre las comillas dobles " para tailwind en las className

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -5,8 +5,18 @@ import icoMovie from '../assets/img/ico-movie.svg';
 import icoTicket from '../assets/img/ico-ticket.svg';
 
 const BottomBar = () => (
-    <nav className='justify-center flex fixed bottom-0 w-full p-4'>
-        <ul className='py-3 px-6 gap-4 flex align-center justify-center rounded-full bg-indigo-600'> 
+    <div className="flex justify-center w-full fixed bottom-0 mb-4">
+    <nav className="flex flex-col gap-3">
+        <div className="bg-indigo-600 rounded-xl p-4 shadow-xl">
+            <ul className="text-center flex flex-col gap-4 text-white text-xl py-5">
+                <li><a href="#">Películas</a></li>
+                <li><a href="#">Actividades</a></li>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Descargas</a></li>
+            </ul>
+        </div>
+
+        <ul className="py-3 px-6 gap-6 flex align-center justify-center rounded-full bg-indigo-600">
             <li><img src={icoCalendar} alt="Calendario" /></li>
             <li><img src={icoHelp} alt="Ayuda" /></li>
             <li><img src={icoBurguer} alt="Menú" /></li>
@@ -14,6 +24,7 @@ const BottomBar = () => (
             <li><img src={icoTicket} alt="Entradas" /></li>
         </ul>
     </nav>
+    </div>
 );
 
 export default BottomBar;
