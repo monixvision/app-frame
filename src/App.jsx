02c688df172d2099270f44a2cea6be4a4f5ea703
monixvision <garciasantanamonica@gmail.com>
2026-05-25 20:16:09 +0200

    hemos cambiado el <></> por BrowserRoutes y sus respectivos hijos Routes y Raoute añadiendo las rutas y los elementos a los que hace referencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import{BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Category from './pages/Category';
 
 const App = () => (
-    <>
+    <BrowserRouter> {/* Esto permite la navegación entre rutas */}
       <Header />
       <main>
-        <Home />
+        <Routes> {/* Aqui se definen las rutas */}
+          <Route index element={<Home />} />  {/* Ruta principal */}
+          <Route path="/peliculas" element={<Category />}/> {/* Ruta para la página de películas que tiene que coincidir en el buscador y justo enseñar el elemento Category solo que la ruta se llama distinto eso no importa le puedes poner el nombre que quieras con tal que coja el elemento en concreto! */}
+        </Routes>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 
 export default App
