e17d6b3e92a4f21ce18dbcf8ead2aad808e106eb
monixvision <garciasantanamonica@gmail.com>
2026-05-27 20:34:19 +0200

    hemos creado el grid para las tarjetas en el modo md y xl

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,14 @@ import btnMas from '../assets/img/btn-more.svg';
 const Card = ({ info }) => {
   const { titulo, imagen, autoria, dia } = info;
   return (
-    <div className="flex border-2 border-zinc-700 mb-4">
+    <div className="col-span-6 lg:col-span-4  flex border-2 border-zinc-700 mb-4 md:mb-0">
       <div className="w-1/2 border-r border-zinc-700">
         <img className="w-full h-full object-cover"
           src={imagen} alt={titulo} />
       </div>
       <div className="w-1/2 p-4 flex flex-col justify-between">
         <div>
-          <h2 className="text-xl mb-2">{titulo}</h2>
+          <h2 className="text-xl mb-2 leading-tight">{titulo}</h2>
           <p className="text-xs"> {autoria}</p>
         </div>
         <div className="flex justify-between items-center">
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,14 +1,14 @@
 import Card from '../components/Card';
 import { CATALOGO_PELIS } from '../data/films';
 
-const Category = () => {
-  return (
-    <section className="grid-catalogo">
+const Category = () => (
+  
+    <div className="container mx-auto p-4 md:grid grid-cols-12 gap-4">
       {CATALOGO_PELIS.map(peli => (
         <Card key={peli.id} info={peli} />
       ))}
-    </section>
-  );
-}
+    </div>
+  
+)
 
 export default Category;
