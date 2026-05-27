d768e544449f75d9f0d81d91db75650087e9a744
monixvision <garciasantanamonica@gmail.com>
2026-05-27 20:40:22 +0200

    hemos adaptado la card al formato vertical cuando esta en md y lg

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,16 @@ import btnMas from '../assets/img/btn-more.svg';
 const Card = ({ info }) => {
   const { titulo, imagen, autoria, dia } = info;
   return (
-    <div className="col-span-6 lg:col-span-4  flex border-2 border-zinc-700 mb-4 md:mb-0">
-      <div className="w-1/2 border-r border-zinc-700">
-        <img className="w-full h-full object-cover"
+    <div className="col-span-6 lg:col-span-4
+    flex border-2 border-zinc-700 mb-4 
+    md:flex-col md:mb-0">
+      <div className="w-1/2 md:w-full md: border-none border-r border-zinc-700">
+        <img className="w-full h-full object-cover aspect-[4/3]"
           src={imagen} alt={titulo} />
       </div>
-      <div className="w-1/2 p-4 flex flex-col justify-between">
+      <div className="w-1/2 md:w-full p-4 flex flex-col justify-between md:gap-4">
         <div>
-          <h2 className="text-xl mb-2 leading-tight">{titulo}</h2>
+          <h2 className="text-xl mb-2 leading-none">{titulo}</h2>
           <p className="text-xs"> {autoria}</p>
         </div>
         <div className="flex justify-between items-center">
