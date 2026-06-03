104a9fb048df1b17e4645a428a4dd7792f5554fb
monixvision <garciasantanamonica@gmail.com>
2026-06-03 21:43:01 +0200

    hemos creado todos los btn de los filtros de dias

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -8,32 +8,55 @@ const Category = () => {
   /* ESTO ES PARA CUANDO HAGAMOS CLICK en todos SE SELECCIONEN y muestren TODAS las peliculas*/
 
   const pelisFiltradas = CATALOGO_PELIS.filter((peli) => {
-  if (diaSeleccionado === 'all') {
-    return true;
-  }
-  return peli.dia === diaSeleccionado;
-})
+    if (diaSeleccionado === 'all') {
+      return true;
+    }
+    return peli.dia === diaSeleccionado;
+  })
 
-return (
-  <div className="container mx-auto p-4 md:grid grid-cols-12 gap-4">
-    <nav className="mb-4">
-      <button className={`text-xs text-white bg-zinc-700 px-4 py-1 rounded-full ${
-        diaSeleccionado === 1
-          ? 'bg-indigo-700'
-          : 'bg-zinz-700 hover:bg-indigo-700'}`} onClick={() => setDiaSeleccionado(1)}>
-        Día 1
-      </button>
-    </nav>
+  return (
+    <div className="container mx-auto p-4 md:grid grid-cols-12 gap-4">
+      <nav className="mb-4 flex gap-4">
+        <button className={`text-xs text-white px-4 py-1 rounded-full ${
+          diaSeleccionado === 'all'
+            ? 'bg-indigo-700'
+            : 'bg-zinc-700 hover:bg-indigo-700'}`} onClick={() => setDiaSeleccionado('all')}>
+          Todo
+        </button>
 
-    {/* hasta aquí hemos creado el filtro para día 1 */}
+        <button className={`text-xs text-white px-4 py-1 rounded-full ${
+          diaSeleccionado === 1
+            ? 'bg-indigo-700'
+            : 'bg-zinc-700 hover:bg-indigo-700'}`} onClick={() => setDiaSeleccionado(1)}>
+          Día 1
+        </button>
 
-    {/* se puede usar nav pq estan usando filtros relacionados con el propio contenido */}
+        <button className={`text-xs text-white px-4 py-1 rounded-full ${
+          diaSeleccionado === 2
+            ? 'bg-indigo-700'
+            : 'bg-zinc-700 hover:bg-indigo-700'}`} onClick={() => setDiaSeleccionado(2)}>
+          Día 2
+        </button>
 
-    {pelisFiltradas.map(peli => (
-      <Card key={peli.id} info={peli} />
-    ))}
-  </div>
+        <button className={`text-xs text-white px-4 py-1 rounded-full ${
+          diaSeleccionado === 3
+            ? 'bg-indigo-700'
+            : 'bg-zinc-700 hover:bg-indigo-700'}`} onClick={() => setDiaSeleccionado(3)}>
+          Día 3
+        </button>
 
-)}
+      </nav>
+
+      {/* hasta aquí hemos creado el filtro para día 1 */}
+
+      {/* se puede usar nav pq estan usando filtros relacionados con el propio contenido */}
+
+      {pelisFiltradas.map(peli => (
+        <Card key={peli.id} info={peli} />
+      ))}
+    </div>
+
+  )
+}
 
 export default Category;
