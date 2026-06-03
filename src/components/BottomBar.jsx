96f4377a960ba3f69a974b42adb017d54fce3f4d
monixvision <garciasantanamonica@gmail.com>
2026-06-03 21:05:44 +0200

    hemos hecho que cuando pases a otra pagina no se quede abierto el burguer

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -14,18 +14,18 @@ const BottomBar = () => {
             <nav className="flex flex-col gap-3">
                 <div className={`bg-indigo-600 rounded-xl p-4 shadow-xl ${isOpen ? 'block' : 'hidden'}`}>
                     <ul className="text-center flex flex-col gap-4 text-white text-xl py-5">
-                        <li><Link to='/peliculas'>Películas</Link></li>
+                        <li><Link to='/peliculas' onClick={() => setIsOpen(!isOpen)}>Películas</Link></li>
                         {/* en el link to tiene que coincidir con la ruta definida en App.jsx */}
-                        <li><Link to='/actividades'>Actividades</Link></li>
-                        <li><Link to='/nosotros'>Nosotros</Link></li>
-                        <li><Link to='/descargas'>Descargas</Link></li>
+                        <li><Link to='/actividades' onClick={() => setIsOpen(!isOpen)}>Actividades</Link></li>
+                        <li><Link to='/nosotros' onClick={() => setIsOpen(!isOpen)}>Nosotros</Link></li>
+                        <li><Link to='/descargas' onClick={() => setIsOpen(!isOpen)}>Descargas</Link></li>
                     </ul>
                 </div>
 
                 <ul className="py-3 px-6 gap-6 flex align-center justify-center rounded-full bg-indigo-600">
-                    <li><Link to='/actividades'><img src={icoCalendar} alt="Calendario" /></Link></li>
-                    <li><Link to='/preguntas'><img src={icoHelp} alt="Ayuda" /></Link></li>
-
+                    <li><Link to='/actividades' onClick={() => setIsOpen(!isOpen)}><img src={icoCalendar} alt="Calendario" /></Link></li>
+                    <li><Link to='/preguntas' onClick={() => setIsOpen(!isOpen)}><img src={icoHelp} alt="Ayuda" /></Link></li>
+  
                     <li>
                         <buttom onClick={() => setIsOpen(!isOpen)} className={`btn-burguer ${isOpen ? 'btn-burguer-cerrado' : ''}`}>
                             <div></div>
@@ -34,8 +34,8 @@ const BottomBar = () => {
                         </buttom>
                     </li>
 
-                    <li><Link to='/peliculas'><img src={icoMovie} alt="Películas" /></Link></li>
-                    <li><Link to='/entradas'><img src={icoTicket} alt="Entradas" /></Link></li>
+                    <li><Link to='/peliculas' onClick={() => setIsOpen(!isOpen)}><img src={icoMovie} alt="Películas" /></Link></li>
+                    <li><Link to='/entradas' onClick={() => setIsOpen(!isOpen)}><img src={icoTicket} alt="Entradas" /></Link></li>
                 </ul>
             </nav>
         </div>
