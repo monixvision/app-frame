bf6123a157845b39f6661fe1346401e16b97b38b
monixvision <garciasantanamonica@gmail.com>
2026-06-08 19:07:17 +0200

    no se que hicimos

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,6 +1,9 @@
+import { HomeIcon } from "@heroicons/react/24/outline";
+
 const Faq = () => (
     <section>
       <h2>FAQ</h2>
+      <HomeIcon className="h-8 w-8 text-red-500" />
       <p>Encuentra respuestas a las preguntas frecuentes...</p>
     </section>
  );
