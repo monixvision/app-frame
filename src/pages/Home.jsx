555c930caa243a14dcf314f2542ad226c041502e
monixvision <garciasantanamonica@gmail.com>
2026-06-08 21:51:03 +0200

    hemos creado el btn generico que se va a ir usando durante toda la app con sus respectivas variantes, sincronizado y con la interactividad para que te lleve a otro lado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
-
+import Button from "../components/Button";
 
 const Home = () => (
 
     <section>
       <h1>Esta es la home</h1>
       <p>Descripción de nuestra aplicación</p>
+      <div className="flex justify-center">
+        <Button to="/peliculas" text="Prueba" variant="outline"/>
+        </div>
+      
+      {/* aqui ponemos el btn con el texto que queremos que tenga y las variantes que queramos y la ruta a la que queremos que le lleve*/}
+
+      {/* para cambiar el css del btn pues simplemente lo contenemos en un div con las propiedades */}
     </section>
  );
 
